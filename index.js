fe457cd2364a1153f293ca0d403c6e894c124d4e
Abdullah al rafi <abdullah116632@gmail.com>
2024-02-03 22:18:06 +0600

    moved all sensitive data to .env file

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config({path: "./config.env"});
+
+
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -14,15 +18,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 
-//ttB3qe6ce01hkIPt
-
-const CONNECTION_URL =
-  "mongodb+srv://abdullah:ttB3qe6ce01hkIPt@cluster0.rktpia1.mongodb.net/post?retryWrites=true&w=majority";
-
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 4000 ;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopoLogy: true })
+  .connect(process.env.CONNECTION_STRING, { useNewUrlParser: true})
   .then(() =>
     app.listen(PORT, () => console.log(`Server is running port:${PORT}`))
   )
