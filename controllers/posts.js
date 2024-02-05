81d24a468aa16e963d52ba3f40245218d556a18e
Abdullah al rafi <abdullah116632@gmail.com>
2024-02-05 22:18:30 +0600

    like api

--- controllers/posts.js
+++ controllers/posts.js
@@ -35,9 +35,9 @@ export const updatePost = async (req, res) => {
   if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No Post with that Id"); //cheacking id is valid or not
    
   
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
 
-  res.json(updatePost);
+  res.json(updatedPost);
 }
 
 export const deletePost = async (req, res) => {
@@ -48,4 +48,15 @@ export const deletePost = async (req, res) => {
   await PostMessage.findByIdAndRemove(id)
 
   res.json({message: "post deleted successfully"});
+}
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No Post with that Id");
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, { new: true });
+
+  res.json(updatedPost);
 }
